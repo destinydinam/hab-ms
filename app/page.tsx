f2ca32b060aaad6621ff6aaf6757650c1fd15a78
destinydinam <destinydinam1@gmail.com>
2024-07-10 11:14:56 +0000

    go to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { urls } from "@/lib/utils";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex items-center justify-center h-full mx-auto p-4">
+      <Link href={urls.admin.doctors}>
+        <Button>Go to Dashboard</Button>
+      </Link>
+    </div>
+  );
 }
